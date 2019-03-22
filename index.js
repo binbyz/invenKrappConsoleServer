a278188329aab7f02e91a645b8f069574a400585
byzz <byzz@inven.co.kr>
2019-03-22 14:52:15 +0900

    tail stream

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
-const express = require('express');
-const http = require('http');
+#!/usr/local/bin/node
+/**
+ * ======================
+ * krapp websocket server
+ * ======================
+ */
+const express   = require('express');
+const http      = require('http');
 const WebSocket = require('ws');
+const fs        = require('fs');
+const tfs       = require('tail-stream');
 
-const app = express();
+const app    = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const wss    = new WebSocket.Server({ server });
+
+const PATH_NGINX_ACCESS_LOG = '/var/log/nginx/access.log';
+const PATH_NGINX_ERROR_LOG  = '/var/log/nginx/error.log';
+const FILE_REFRESH_INTERVAL = 100;
+
+let sessions = [];
+let streams = [];
+
+streams.push(tfs.createReadStream(PATH_NGINX_ACCESS_LOG, { interval: FILE_REFRESH_INTERVAL }));
+streams.push(tfs.createReadStream(PATH_NGINX_ERROR_LOG, { interval: FILE_REFRESH_INTERVAL }));
+
+for (let f in streams) {
+  streams[f].on('data', (chunk) => {
+    let decoded = chunk.toString();
+
+    if (sessions.length) {
+      for (let s in sessions) {
+        let socket = sessions[s];
+
+        if (socket.readyState === 1) {
+          socket.send(decoded);
+        }
+      }
+    }
+  });
+}
+
+wss.broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    client.send(data);
+  });
+};
 
 wss.on('connection', (ws) => {
-  ws.on('message', (receiv) => {
-    console.log(`received: ${receiv}`);
-    ws.send(`Hello, you send -> ${receiv}`);
+  sessions.push(ws);
+
+  ws.on('message', (recv) => {
+    console.log(`received: ${recv}`);
+    ws.send(`Hello, you send -> ${recv}`);
   });
 });
 
